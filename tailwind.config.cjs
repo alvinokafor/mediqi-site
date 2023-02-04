0a6dc4e54323ea641b0792f015679ec6f17d97ee
Alvin Okafor <alvin2k99@gmail.com>
2023-02-04 22:28:55 +0100

    feat: header & nav section added

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Plus Jakarta Sans", "sans-serif"],
+        heading: ["Sporting Grotesque", "serif"],
+      },
+      colors: {
+        primary: "#1D2238",
+        secondary: "#465286",
+      },
+      fontSize: {
+        "heading-one": "3.375rem",
+        "heading-two": "2.5rem",
+        "heading-three": "1.625rem",
+      },
+      maxWidth: {
+        container: "1290px",
+      },
+    },
+  },
+  plugins: [],
+};
